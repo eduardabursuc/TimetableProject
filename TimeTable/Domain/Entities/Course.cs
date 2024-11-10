793ffa1103179013d09e978c75953402abed4f45
Eduarda <117537516+eduardabursuc@users.noreply.github.com>
2024-11-10 16:59:18 +0200

    Update Course.cs

--- TimeTable/Domain/Entities/Course.cs
+++ TimeTable/Domain/Entities/Course.cs
@@ -2,13 +2,10 @@
 {
     public class Course
     {
-        public string CourseName { get; set; }
-        public int Credits { get; set; }
-        public string Package { get; set; }
-        public int Semester { get; set; }
-        public string Level { get; set; }
-
-        // Many-to-Many relationship with Professor
-        public List<Professor> Professors { get; set; } = new List<Professor>();
+        public required string CourseName { get; set; }
+        public required int Credits { get; set; }
+        public required string Package { get; set; }
+        public required int Semester { get; set; }
+        public required string Level { get; set; }
     }
 }
