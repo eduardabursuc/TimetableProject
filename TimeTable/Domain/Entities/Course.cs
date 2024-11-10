11da559950b76181b9d9cc5d08b8b44001ba0882
Eduarda <117537516+eduardabursuc@users.noreply.github.com>
2024-11-10 16:22:21 +0200

    Update Course.cs

--- TimeTable/Domain/Entities/Course.cs
+++ TimeTable/Domain/Entities/Course.cs
@@ -2,7 +2,6 @@
 {
     public class Course
     {
-<<<<<<< Updated upstream
         public string CourseName { get; set; }
         public int Credits { get; set; }
         public string Package { get; set; }
@@ -11,12 +10,5 @@
 
         // Many-to-Many relationship with Professor
         public List<Professor> Professors { get; set; } = new List<Professor>();
-=======
-        public required string CourseName { get; set; }
-        public required int Credits { get; set; }
-        public required string Package { get; set; }
-        public required int Semester { get; set; }
-        public required string Level { get; set; }
->>>>>>> Stashed changes
     }
 }
