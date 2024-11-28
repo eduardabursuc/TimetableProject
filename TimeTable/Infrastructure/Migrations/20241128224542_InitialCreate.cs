c60267b5739cf50a597440d99bc2b7bb9ac07abb
simonaAMH <amihaesiisimona5@gmail.com>
2024-11-29 00:48:55 +0200

    new migration

---
+++ TimeTable/Infrastructure/Migrations/20241128224542_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_timeslots",
+                table: "timeslots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_timeslots_TimetableId",
+                table: "timeslots");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_timeslots",
+                table: "timeslots",
+                columns: new[] { "TimetableId", "Time", "Day", "RoomName" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_timeslots",
+                table: "timeslots");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_timeslots",
+                table: "timeslots",
+                columns: new[] { "Time", "Day", "RoomName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_timeslots_TimetableId",
+                table: "timeslots",
+                column: "TimetableId");
+        }
+    }
+}
