87b1a070e5c4d6cd19b800346e7802f2407f0535
Eduarda <117537516+eduardabursuc@users.noreply.github.com>
2024-12-03 10:58:07 +0200

    Task: add last changes

--- TimeTable/TimeTable/Program.cs
+++ TimeTable/TimeTable/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("https://timetable-project.vercel.app")
               .AllowAnyHeader()                    
               .AllowAnyMethod();                   
     });
